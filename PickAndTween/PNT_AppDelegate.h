20f88998da542a59a7587e4a49e02c3c6f3c9218
awalin sopan <awalinnabila@gmail.com>
2014-07-01 15:05:53 -0400

    Initial Commit

---
+++ PickAndTween/PNT_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PNT_AppDelegate.h
+//  PickAndTween
+//
+//  Created by Sopan, Awalin on 7/1/14.
+//  Copyright (c) 2014 __mstr__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PNT_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PickAndTween/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PickAndTween
+//
+//  Created by Sopan, Awalin on 7/1/14.
+//  Copyright (c) 2014 __mstr__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PNT_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PNT_AppDelegate class]));
+    }
+}
